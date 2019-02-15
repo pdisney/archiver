75367fce5be139d2be1e1084ca874a99fc58c96e
Paul_Disney <paul_disney@epam.com>
2019-02-15 11:14:14 -0500

    updated

--- processors/URLDocument.js
+++ processors/URLDocument.js
@@ -1,25 +1,25 @@
-class URLDocument{
-    constructor(domain, url, timestamp, ipAddresses, html, ocr, entities, products, relationships, links){
-        
+class URLDocument {
+    constructor(domain, url, timestamp, ipAddresses, html, ocr, entities, products, relationships, links) {
+
         this.domain = domain;
         this.url = url;
         this.timestamp = timestamp;
-        this.ipAddresses=[];
-        if(ipAddresses)
+        this.ipAddresses = [];
+        if (ipAddresses)
             this.ipAddresses = ipAddresses;
-        this.ocr =[];
-        if(ocr)
+        this.ocr = [];
+        if (ocr)
             this.ocr = ocr;
-        this.images=[];
-        this.entities=[];
-        if(entities)
-            this.entities= entities;
-        this.products=[];
-        if(products)
+        this.images = [];
+        this.entities = [];
+        if (entities)
+            this.entities = entities;
+        this.products = [];
+        if (products)
             this.products = products;
-        this.relationships=[];
-        if(relationships)
-            this.relationships=relationships;
+        this.relationships = [];
+        if (relationships)
+            this.relationships = relationships;
         this.links = links;
         this.html = html;
         return this;
