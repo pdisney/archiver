726071ccca15c348a5bd30145231260f924473d3
Paul_Disney <paul_disney@epam.com>
2019-02-25 16:09:49 -0500

    Fixed issue with missing environment variable.

--- common/global_init.js
+++ common/global_init.js
@@ -29,7 +29,7 @@ var databaseInit = async () => {
 
 var mqInit = async () => {
   console.info("Initializing MQ Client");
-  global.config.mq_address = process.env.MQ_ADDRESS
+  global.config.mq_address = process.env.MQ_ADDRESS;
   global.mq_connector = await new RabbitConnector(global.config.mq_address);
   global.queues = {};
   global.queues.url_archive = 'url_archive_process';
@@ -39,21 +39,29 @@ var mqInit = async () => {
   return;
 };
 
-var appInit = async () => {
-  if (!global.config) {
-    global.config = {};
-  }
+var azureInit = async () => {
+
+  global.AzureKey = process.env.AZUREKEY;
+  global.AzureCoolKey = process.env.AZURECOOLKEY;
+  global.AzureCoolBucket = process.env.AZURECOOLBUCKET;
+  global.AzureCoolStorageAccount = process.env.AZURECOOLSTORAGEACCOUNT;
+
+  global.AzureDownload = new AzureStorageWrapper();
+  global.AzureUpload = new AzureBlobSaver();
+  console.info("AZURE Clients Inititialized");
+}
+
+var awsInit = async () => {
   global.SecretKey = process.env.SECRETKEY;
   global.AccessKey = process.env.ACCESSKEY;
   global.S3Bucket = process.env.BUCKET;
   global.Region = process.env.REGION;
+}
 
-  global.AzureKey = process.env.AZUREKEY;
-  global.AzureCoolKey = process.env.AZURECOOLKEY;
-  global.AzurecoolBucket = process.env.AZURECOOLBUCKET;
-
-  global.AzureDownload =  new AzureStorageWrapper();
-  global.AzureUpload = new AzureBlobSaver();
+var appInit = async () => {
+  if (!global.config) {
+    global.config = {};
+  }
 
   console.info("General App Initialized");
 
@@ -62,4 +70,5 @@ var appInit = async () => {
 
 
 module.exports.globalInit = globalInit;
+module.exports.azureInit = azureInit;
 module.exports.appInit = appInit;
