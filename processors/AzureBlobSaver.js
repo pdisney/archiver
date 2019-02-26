6fd35394b66f0a64db95721acf2c7234554d0282
Paul_Disney <paul_disney@epam.com>
2019-02-26 11:03:34 -0500

    Updated to use streams to upload due to 64MB file limit.

--- processors/AzureBlobSaver.js
+++ processors/AzureBlobSaver.js
@@ -1,4 +1,5 @@
 const azure_storage = require('azure-storage');
+var Readable = require('stream').Readable;
 
 
 const createContainer = async (blobService, container) => {
@@ -13,9 +14,79 @@ const createContainer = async (blobService, container) => {
     });
 };
 
-const upload = async (blobService, container, filename, data) => {
+const getAzureWriteStream = async (blobService, container, filename, content_type) => {
+    return new Promise((resolve) => {
+        return resolve(blobService.createWriteStreamToBlockBlob(
+            container,
+            filename,
+            { contentType: content_type },//content_type'image/jpeg' },
+            (err, result, response) => {
+                if (err) {
+                    console.error("Couldn't upload file %s ", filename, err);
+
+                } else {
+                    console.debug('File %s uploaded to ', filename, container);
+                }
+            }));
+    });
+}
+
+const deleteBlob = async (blobService, container, filename) => {
     return new Promise((resolve, reject) => {
-        blobService.createBlockBlobFromText(container, filename, data, err => {
+        blobService.deleteBlobIfExists(container, filename, err => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve({ message: `Block blob '${filename}' deleted` });
+            }
+        });
+    });
+};
+
+var uploadFile = (blobService, container, filename, document, content_type) => {
+    return new Promise((resolve, reject) => {
+
+        var documentStream = new Readable();
+        documentStream.push(document);
+        documentStream.push(null);
+
+        getAzureWriteStream(blobService, container, filename, content_type).then(writeStream => {
+
+            var dataLength = 0;
+            documentStream.on('data', (data) => {
+                dataLength += data.length;
+                writeStream.write(data);
+            });
+            documentStream.on('end', () => {
+                writeStream.end();
+            });
+            documentStream.on('error', (err) => {
+                console.error("AzureWriteStream Error", err);
+                return reject(err);
+            });
+
+            writeStream.on('error', (err) => {
+                return reject(err);
+            });
+            writeStream.on('end', () => {
+                console.info("[AzureWrapper] Image streaming to " + container + " complete: " + filename + " dataLength:" + dataLength);
+                if (dataLength === 0) {
+                    deleteBlob(blobService, container, filename).then(() => {
+                        return resolve(0);
+                    }).catch(err => {
+                        return reject(err);
+                    })
+                } else {
+                    return resolve(dataLength);
+                }
+            });
+        });
+    });
+}
+/*
+const upload = (blobService, container, filename, data) => {
+    return new Promise((resolve, reject) => {
+       blobService.createBlockBlobFromText(container, filename, data, err => {
             if (err) {
                 reject(err);
             } else {
@@ -24,7 +95,7 @@ const upload = async (blobService, container, filename, data) => {
         });
     });
 };
-
+*/
 
 
 class AzureBlobSaver {
@@ -36,7 +107,7 @@ class AzureBlobSaver {
         return (async () => {
             try {
                 await createContainer(this.blobService, global.AzureCoolBucket);
-                await upload(this.blobService, global.AzureCoolBucket, filename, JSON.stringify(document));
+                await uploadFile(this.blobService, global.AzureCoolBucket, filename, JSON.stringify(document), "application/json");
             } catch (err) {
                 console.error(err);
                 throw err;
